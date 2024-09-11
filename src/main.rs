deda42b770abfc96546641189fa8289e67bcc335
“Travis <t.vanderstad@gmail.com>
2024-09-11 19:37:24 -0400

    scroll area interaction poc 2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use eframe::egui::{self, scroll_area, Id, Sense};
+use eframe::egui::{self, scroll_area, CentralPanel, Frame, Pos2, Rect, Sense};
 
 struct MyApp {}
 
@@ -19,28 +19,39 @@ fn main() {
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            // show ui
-            let maybe_prev_state: Option<scroll_area::State> =
-                ctx.data_mut(|d| d.get_persisted(ui.id().with(Id::new("id"))));
-            let scroll_area_output = egui::ScrollArea::vertical().id_source("id").show(ui, |ui| {
-                for i in 0..100 {
-                    ui.label(format!("label {}", i));
+        CentralPanel::default()
+            .frame(Frame {
+                fill: ctx.style().visuals.panel_fill,
+                ..Default::default() // removes default margins
+            })
+            .show(ctx, |ui| {
+                ctx.style_mut(|s| s.spacing.item_spacing = egui::vec2(0.0, 0.0));
+
+                let available_size = ui.available_size();
+
+                // show ui
+                let scroll_area_id = ui.id().with(egui::Id::new("id"));
+                let maybe_prev_state: Option<scroll_area::State> =
+                    ui.data_mut(|d| d.get_persisted(scroll_area_id));
+
+                let scroll_area_output =
+                    egui::ScrollArea::vertical().id_source("id").show(ui, |ui| {
+                        for i in 0..100 {
+                            ui.label(format!("label {}", i));
+                        }
+
+                        let rect = Rect::from_min_size(Pos2::ZERO, available_size);
+                        let response = ui.allocate_rect(rect, Sense::click());
+                        if let Some(pos) = response.interact_pointer_pos() {
+                            println!("clicked {:?}", pos);
+                        };
+                    });
+
+                if let Some(prev_state) = maybe_prev_state {
+                    if prev_state.offset != scroll_area_output.state.offset {
+                        println!("scrolled");
+                    }
                 }
             });
-
-            if let Some(prev_state) = maybe_prev_state {
-                if prev_state.offset != scroll_area_output.state.offset {
-                    println!("scrolled");
-                }
-            }
-
-            if let Some(pos) = ui
-                .allocate_rect(scroll_area_output.inner_rect, Sense::click())
-                .interact_pointer_pos()
-            {
-                println!("clicked {:?}", pos);
-            };
-        });
     }
 }

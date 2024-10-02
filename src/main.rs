5e6eae41962a0cc3638050d4d461b7ee88ecdbbb
“Travis <t.vanderstad@gmail.com>
2024-10-02 10:36:16 -0400

    read response poc

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use eframe::egui::{self, CentralPanel, Frame};
+use eframe::egui::{self, CentralPanel, Color32, Frame, Id, Rect, Rounding, Sense, Stroke, Vec2};
 
 struct MyApp {
-    button_id: Option<egui::Id>,
+    rect_id: Option<egui::Id>,
 }
 
 impl MyApp {
     fn new() -> Self {
-        Self { button_id: None }
+        Self { rect_id: None }
     }
 }
 
@@ -27,15 +27,38 @@ impl eframe::App for MyApp {
                 ..Default::default() // removes default margins
             })
             .show(ctx, |ui| {
+                println!("----- show -----");
                 ctx.style_mut(|s| s.spacing.item_spacing = egui::vec2(0.0, 0.0));
 
-                if self.button_id.is_some()
-                    && ctx.interaction_snapshot(|r| r.clicked) == self.button_id
-                {
-                    println!("clicked");
+                if let Some(rect_id) = self.rect_id {
+                    if let Some(response) = ctx.read_response(rect_id) {
+                        if response.clicked() {
+                            println!("response reported CLICKED on next frame");
+                        }
+                        if response.hovered() {
+                            println!("response reported HOVERED on next frame");
+                        }
+                    }
                 }
 
-                self.button_id = Some(ui.button("button").id);
+                let id = Id::new(0);
+                let (_, rect) = ui.allocate_space(Vec2::new(100.0, 100.0));
+                let response = ui.interact(rect, id, Sense::click_and_drag());
+
+                ui.painter().rect(
+                    rect,
+                    Rounding::ZERO,
+                    Color32::TRANSPARENT,
+                    Stroke::new(2., Color32::BLUE),
+                );
+
+                self.rect_id = Some(response.id);
+                if response.clicked() {
+                    println!("response reported CLICKED on frame drawn");
+                }
+                if response.hovered() {
+                    println!("response reported HOVERED on frame drawn");
+                }
             });
     }
 }
